655837b2ef01c89d7638142dd832a893faceed88
Lucifer921340 <kashishkumarverma265@gmail.com>
2026-05-02 23:13:41 +0530

    debug some code

---
+++ EvService/src/pages/ServicesDetailsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ServicesDetails from '../components/ServicesDetails'
+
+const ServiceDetailsPage = () => {
+    return (
+        <div>
+            <ServicesDetails />
+        </div>
+    )
+}
+
+export default ServiceDetailsPage
